f939d58cf0506ea738c2a0191f1729c5d731c7c1
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-09-11 17:10:32 +0200

    add random email method

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 const { createApp } = Vue
 
 createApp({
+
     data() {
         return {
 
+            apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+
         }
-    }
+    },
+
+    methods: {
+
+        getRandomEmail() {
+            axios.get(this.apiUrl)
+
+                .then(response => {
+                    console.log(response.data.response);
+                })
+        },
+
+    },
+
+    mounted() {
+        this.getRandomEmail();
+    },
+
 }).mount('#app')
+
